fd39d8894bc462254bcd25010ecb3302e2e1ddb7
rakibul islam <rakib016477@gmail.com>
2023-08-05 22:21:02 +0600

    commit3

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 //active navebar
-
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("scroll-on");
+  } else {
+    nav.classList.remove("scroll-on");
+  }
+};
 //nav hide
-
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
 //ccxounter dasign
 document.addEventListener("DOMContentLoaded", () => {
   function counter(id, start, end, duration) {
